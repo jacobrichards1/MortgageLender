8eb5be5f174db5a58365d5e5bd7db2046506268b
rahmin123 <56046345+rahmin123@users.noreply.github.com>
2021-10-11 10:45:03 -0500

    Update LoanService.java
    Comments

--- mortgageLender/src/main/java/mortgageLender/LoanService.java
+++ mortgageLender/src/main/java/mortgageLender/LoanService.java
@@ -10,23 +10,23 @@ public class LoanService {
 	Lender lender = new Lender();
 	
 	
-	public void addFunds(int amt) {
+	public void addFunds(int amt) { //Adds money to availableFunds
 		lender.setAvailableFunds(lender.getAvailableFunds() + amt);
 	}
 	
-	public int getLenderFunds() {
+	public int getLenderFunds() { //gets AvailableFunds
 		return lender.getAvailableFunds();
 	}
 	
-	public int getPendingFunds() {
+	public int getPendingFunds() { //gets PendingFunds
 		return lender.getPendingFunds();
 	}
 	
-	public List<Loan> getAllLoans(){
+	public List<Loan> getAllLoans(){ //gets all the loan applications
 		return lender.getCurLoans();
 	}
 	
-	public Loan createLoan(int request, int dti, int credit_score, int savings, Date d){
+	public Loan createLoan(int request, int dti, int credit_score, int savings, Date d){ //creates loan applications
 		List<Loan> ls = new ArrayList<Loan>();
 		
 		if (!(lender.getCurLoans() == null)) {
@@ -36,7 +36,7 @@ public class LoanService {
 		}
 		
 		int lID; 
-		if (lender.getCurLoans() == null) {
+		if (lender.getCurLoans() == null) { //assigns loanID
 			lID = 1;
 		}
 		else {
@@ -46,7 +46,7 @@ public class LoanService {
 		
 		Loan l = new Loan(lID, request, dti, credit_score, savings, d);
 		int qual = -1;
-		if (request <= lender.getAvailableFunds() && dti < 36 && credit_score > 620) {			
+		if (request <= lender.getAvailableFunds() && dti < 36 && credit_score > 620) {	//Assigns qualification to each loan		
 			if (savings * 4 >= request) {
 				qual = 1;
 				l.setStatus("qualified");
@@ -67,50 +67,50 @@ public class LoanService {
 		return l;
 	}
 
-	public void getApprovedLoans(){
+	public void getApprovedLoans(){ //Gets loans that are fully qualified and partially qualified
 		int i = 0;
 		for (Loan l : lender.getCurLoans()) {
 			if (l.getQualification() == 1 || l.getQualification() == 0) {				
 					if (l.getQualification() == 0) {
 						i = l.getSavings() * 4;
 						l.setLoanAmount(i);	
-						l.setStatus("approved");
+						l.setStatus("approved"); //Application is approved if it meets all the requirements
 						if (lender.getAvailableFunds() - i >= 0) {
 							lender.setAvailableFunds(lender.getAvailableFunds() - i);
 							lender.setPendingFunds(lender.getPendingFunds() + i);
 						}
 						else {
-							l.setStatus("on hold");
+							l.setStatus("on hold"); //Puts application on hold if there isn't enough money in availableFunds
 						}
 						
 					}
 					else {
 						i = l.getRequestedAmount();
 						l.setLoanAmount(i);
-						l.setStatus("approved");
+						l.setStatus("approved"); //Application is approved if it meets all the requirements
 						if (lender.getAvailableFunds() - i >= 0) {
 							lender.setAvailableFunds(lender.getAvailableFunds() - i);
 							lender.setPendingFunds(lender.getPendingFunds() + i);
 						}
 						else {
-							l.setStatus("on hold");
+							l.setStatus("on hold"); //Puts application on hold if there isn't enough money in availableFunds
 						}
 					} 
 				
 		}
 			else {
 				l.setLoanAmount(0);
-				l.setStatus("denied");
+				l.setStatus("denied"); //If application does not meet all the requirements, it is denied
 			}
 			
 		}
 		
 	}
 	
-	public void approveOnHoldLoan(Loan l) {
+	public void approveOnHoldLoan(Loan l) { //Checks loans "on hold" and if there is enough availableFund, changes status to approved
 		int i = 0;
 	
-		if (l.getQualification() == 0) {
+		if (l.getQualification() == 0) { //Checks for partial qualification and changes the loanAmount
 			i = l.getSavings() * 4;
 			l.setLoanAmount(i);	
 			if (lender.getAvailableFunds() - i >= 0) {
@@ -119,7 +119,7 @@ public class LoanService {
 				lender.setPendingFunds(lender.getPendingFunds() + i);
 			}
 		}
-		else {
+		else { //If its fully qualified
 			i = l.getRequestedAmount();
 			l.setLoanAmount(i);
 			if (lender.getAvailableFunds() - i >= 0) {
@@ -130,8 +130,7 @@ public class LoanService {
 		}
 	}
 	
-	public void appOrDen (boolean[] input) {
-		
+	public void appOrDen (boolean[] input) { //Checks if customer accepts or rejects offer
 		
 		int i = 0;
 		for (Loan l : lender.getCurLoans()) {
@@ -140,17 +139,18 @@ public class LoanService {
 			
 			l.setCustomerResponseToTheQuestionOfWouldTheyLikeTheLoanOrNot(input[i]);
 			
-			if (b) {
+			if (b) { //Checks to see if offer has expired and puts the money from pendingAmount to availableAmount
 				lender.setAvailableFunds(lender.getAvailableFunds() + l.getLoanAmount());
 				lender.setPendingFunds(lender.getPendingFunds() - l.getLoanAmount());
 				i++;
 				continue;
 			}
-			else if (l.getStatus().equals("approved") && l.getCustomerResponseToTheQuestionOfWouldTheyLikeTheLoanOrNot()) {
+			else if (l.getStatus().equals("approved") && l.getCustomerResponseToTheQuestionOfWouldTheyLikeTheLoanOrNot()) { 
+				//changes to accepted status and completes the transaction
 					l.setStatus("accepted");			
 					lender.setPendingFunds(lender.getPendingFunds() - l.getLoanAmount());
 			}
-			else {
+			else { //changes to reject status and puts the appropriate pendingFunds back to availableFunds
 				l.setStatus("rejected");
 				lender.setAvailableFunds(lender.getAvailableFunds() + l.getLoanAmount());
 				lender.setPendingFunds(lender.getPendingFunds() - l.getLoanAmount());
@@ -167,7 +167,7 @@ public class LoanService {
 	
 	
 	
-	public boolean checkExpiration(Loan lo) {
+	public boolean checkExpiration(Loan lo) { //Checks if 3 days have passed since loanDate
 		Calendar c1 = Calendar.getInstance();
 		Calendar c2 = Calendar.getInstance();
 		
@@ -182,7 +182,7 @@ public class LoanService {
 		
 	}
 	
-	public List<Loan> filterStatus (String st){
+	public List<Loan> filterStatus (String st){ //gets the loan Status
 		List<Loan> ls = new ArrayList<Loan>();
 		for (Loan l : lender.getCurLoans()) {
 			if (l.getStatus() == st) {
